5cb406f36bd5dae761c6b1547deeb1c9e333d665
thabisile <thabisilemkhize81@gmail.com>
2023-02-20 13:24:29 -0800

    Numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	putchar('\n');
+		return (0);
+}
