fafe7763f2f17d7076e647b1deffcbefbe64160a
thabisile <thabisilemkhize81@gmail.com>
2023-02-20 12:14:01 -0800

    I suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while
+		(letter <= 'z') {
+			putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
