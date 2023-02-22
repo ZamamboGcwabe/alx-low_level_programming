518274b30cf51eb31f93a57f52eb4514e8597448
thabisile <thabisilemkhize81@gmail.com>
2023-02-22 12:21:37 -0800

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,10 +8,12 @@ int main(void)
 {
 	int fib[50];
 
+	int i;
+
 	fib[0] = 1;
 	fib[1] = 2;
 	printf("%d, %d", fib[0], fib[1]);
-	for (int i = 2; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 		printf(", %d", fib[i]);
