42155feb9bd7dc7ab11667d1fcf85eae5fa47cc4
thabisile <thabisilemkhize81@gmail.com>
2023-02-22 03:14:50 -0800

    Multiple problems

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,15 +2,15 @@
 
 /**
  *positive_or_negative - prints correct output when given 0
- *@i: int to be checked
+ *@n: int to be checked
  *Return: always 0 (success)
  */
-void positive_or_negative(int i)
+void positive_or_negative(int n)
 {
-	if (i == 0)
-		printf("%d is zero.\n", i);
-	else if (i > 0)
-		printf("%d is positive.\n", i);
+	if (n == 0)
+		printf("%d is zero.\n", n);
+	else if (n > 0)
+		printf("%d is positive.\n", n);
 	else
-		printf("%d is negtive.\n", i);
+		printf("%d is negtive.\n", n);
 }
