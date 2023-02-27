97e9a6d830edd5e661e730c7764ba3ca90b100c6
thabisile <thabisilemkhize81@gmail.com>
2023-02-27 11:05:47 -0800

    This report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_strlen - returns the length of a string
+  *@s: char to be printed
+  *Return: length of string
+  */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while
+		(*s != '\0');
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
