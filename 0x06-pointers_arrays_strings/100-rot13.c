26a2c1ce995956e8094e2c41aa4489096498991c
thabisile <thabisilemkhize81@gmail.com>
2023-03-01 10:47:08 -0800

    Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *rot13 - encodes a string using rot13
+  *@str: pointer to string
+  *Return: Always 0 (success)
+  */
+char *rot13(char *str)
+{
+	int i, length = 0;
+	char *result;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	result = (char *)malloc(sizeof(char) * (length + 1));
+
+	for (i = 0; i < length; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			result[i] = (((str[i] - 'a') + 13) % 26) + 'a';
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			result[i] = (((str[i] - 'A') + 13) % 26) + 'A';
+		}
+		else
+		{
+			result[i] = str[i];
+		}
+	}
+
+	result[i] = '\0';
+	return (0);
+}
