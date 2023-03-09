9245ea999efdef6b506b04a8167d60acec7f4bce
thabisile <thabisilemkhize81@gmail.com>
2023-03-08 17:10:22 -0800

    Dreams feel real

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *_strlen_recursion - returns the length of a string
+  *@s: strint to be printed
+  *Return: always 0 (success)
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	_strlen_recursion(s + 1);
+	return (1);
+}
