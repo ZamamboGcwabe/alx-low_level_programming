23ad1a12a7adb7d4411169966c2d320850fcb8e3
thabisile <thabisilemkhize81@gmail.com>
2023-03-10 13:10:10 -0800

    They call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,18 +5,11 @@
   *main - prints its name
   *@argc: indicates number of command line
   *@argv: array of strings
+  *
   *Return: always 0 (success)
   */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i = 1;
-
-	printf("%d\n", argc);
-	printf("%s\n", argv[0]);
-
-	for (; i < argc; i++)
-	{
-		printf("Argument %d: %s\n", i, argv[i]);
-	}
+	printf("%s\n", *argv);
 	return (0);
 }
