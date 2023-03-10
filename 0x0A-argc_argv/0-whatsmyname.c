85d094ce2230f865c94c9b9ca15c483adef5dd4b
thabisile <thabisilemkhize81@gmail.com>
2023-03-10 12:45:00 -0800

    It aint what they call

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,16 @@
   *@argv: array of strings
   *Return: always 0 (success)
   */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+	int i = 1;
+
+	printf("%d\n", argc);
 	printf("%s\n", argv[0]);
+
+	for (; i < argc; i++)
+	{
+		printf("Argument %d: %s\n", i, argv[i]);
+	}
 	return (0);
 }
