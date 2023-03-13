fe8fd33529fad458816196a7bf21872e9fa1508d
thabisile <thabisilemkhize81@gmail.com>
2023-03-13 09:23:02 -0700

    He who is not courageous

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  *str_concat - concatenates two strings
+  *@s1: ointer to a string
+  *@s2: pointer to a string
+  *Return: NULL on failure
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *result = malloc(len1 + len2 + 1);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(result, s1);
+	strcat(result, s2);
+	return (result);
+}
