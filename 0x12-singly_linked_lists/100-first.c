629036183be14d527c3abdaa8bebee834c84bccf
Thabisile <thabisilemkhize@gmail.com>
2023-03-30 14:14:01 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  *print_intro - prints an intto before the main function is executed
+  *Return: Always 0 (success)
+  */
+void print_intro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
