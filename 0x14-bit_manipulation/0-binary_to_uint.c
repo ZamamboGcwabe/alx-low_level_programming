f85193a607a9f2db03d26d3e6c1ef9dbb1b16720
Thabisile <thabisilemkhize@gmail.com>
2023-05-05 11:17:12 -0700

    Binary to unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *binary_to_unit - converts a binary number to an unsigned int
+ *@b: number to convert
+ *Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int len;
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	len = strlen(b);
+	for (i = 0; i < len; i++);
+	{
+		if (b[i] == '0')
+		{
+			result <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			result <<= 1;
+			result |= 1;
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
