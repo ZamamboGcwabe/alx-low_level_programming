fb157cac9e0269c915d3674a1c62f94d6d73aa03
Thabisile <thabisilemkhize81@gmail.com>
2023-08-21 05:11:00 -0700

    A library is not a luxury

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_strpbrk - searches a string for any of a set of bytes
+  *@s: pointer to a string
+  *@accept: pointer to a string
+  *Return: NULL
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p = accept;
+
+	while (*s != '\0')
+	{
+		for (; *p != '\0'; p++)
+		{
+			if (*s == *p)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
